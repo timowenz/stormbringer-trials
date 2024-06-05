47649ee9382935544778b4a7d0ff9367127fde1a
Niels Kochert <48764692+vescito@users.noreply.github.com>
2024-06-05 10:10:27 +0200

    big progress

--- scripts/Weather.gd
+++ scripts/Weather.gd
@@ -1,8 +1,11 @@
 extends Node
 
+var isRaining : bool
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	isRaining = false
 	pass # Replace with function body.
 
 
@@ -11,4 +14,17 @@ func _process(delta):
 	pass
 
 func changeWeather():
+	print("Changing weather")
+	if(isRaining):
+		%Rain.visible = false
+		isRaining = false
+	else:
+		%Rain.visible = true
+		isRaining = true
+	$"../Sapling".changeGrowth()
 	pass
+
+
+func _on_weather_button_pressed():
+	changeWeather()
+	pass # Replace with function body.
