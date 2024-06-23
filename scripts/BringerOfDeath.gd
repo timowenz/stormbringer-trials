c5d52cc161ff90f8ea12f5b881bbb11d8ff6762f
Timo Wenz <2025014@stud.hs-mannheim.de>
2024-06-23 11:22:02 +0200

    feat: added BringerOfDeath enemy to game

---
+++ scripts/BringerOfDeath.gd
@@ -0,0 +1,64 @@
+extends CharacterBody2D
+
+const SPEED = 150
+const GRAVITY = 25
+const JUMP_HEIGHT = 400
+var health = 200
+var player = null
+var player_chase = false
+var damage = 15
+signal dead
+
+@onready var healthbar = $HealthBar
+
+func _ready():
+	healthbar.init_health(health)
+
+func _physics_process(_delta):
+	velocity.y += GRAVITY
+
+	if (player_chase):
+
+		var direction = (player.position - position).normalized()
+		velocity.x = direction.x * SPEED
+
+		if (player.position.x < position.x):
+			$AnimatedSprite2D.flip_h = false
+			$AnimatedSprite2D.offset.x = 0
+			$Node2D.scale.x = 1
+		else:
+			$AnimatedSprite2D.flip_h = true
+			# This is because the sprite: 'BringerOfDeath' is not centered
+			$AnimatedSprite2D.offset.x = 60
+			$Node2D.scale.x = -1
+		
+		if (player.position.distance_to(position) < 50):
+			$AnimatedSprite2D.play("attack")
+		else:
+			$AnimatedSprite2D.play("walk")
+	move_and_slide()
+
+func _on_detection_area_2d_body_entered(body):
+	player = body
+	player_chase = true
+
+func get_health():
+	return health
+
+func set_health(value):
+	health = value
+
+func take_damage(damage):
+	health -= damage
+	healthbar.health = health
+	if (health <= 0):
+		dead.emit()
+		queue_free()
+
+func _on_animated_sprite_2d_animation_finished():
+	player.take_damage(damage)
+
+func _on_area_2d_body_entered(body):
+	# jump
+	if (body.name == "TileMap"):
+		velocity.y = -JUMP_HEIGHT
