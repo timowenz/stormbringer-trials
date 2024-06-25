615ad39e5f79fcf3fb1baade4179e538bdf2635b
Niels Kochert <kochertN@gmail.com>
2024-06-25 16:47:17 +0200

    reset

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
 #var state_machine
-var SPEED = 190.0
+const SPEED = 190.0
 const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var direction2 : Vector2 = Vector2.ZERO
+var direction: float = 0
+var direction2: Vector2 = Vector2.ZERO
 # Animation variables
 @onready var anim = get_node("AnimationPlayer")
 @onready var anim_tree: AnimationTree = $AnimationTree
@@ -76,10 +77,9 @@ func _physics_process(delta):
 	#elif not is_on_floor() and velocity.y < 0 and can_jump:
 		#anim.play("jump")
 	
-	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	direction = Input.get_axis("ui_left", "ui_right")
 	if direction == - 1 and is_attacking == false:
 		get_node("AnimatedSprite2D").flip_h = true
 		$Node2D.scale = Vector2( - 1, 1)
@@ -97,7 +97,7 @@ func _physics_process(delta):
 					velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	direction2 = $Node2D.scale
+	##direction2 = $Node2D.scale
 	
 	move_and_slide()
 
@@ -168,7 +168,6 @@ func update_anim_params():
 						jump()
 					else:
 						run()
-
 	
 	if Input.is_action_just_pressed("attack"):
 		if is_crouching:
@@ -212,7 +211,6 @@ func crouch_walk():
 	anim_tree["parameters/conditions/is_crouch_walking"] = true
 	anim_tree["parameters/conditions/crouch"] = false
 
-
 func idle():
 	anim_tree["parameters/conditions/idle"] = true
 	anim_tree["parameters/conditions/is_moving"] = false
@@ -255,7 +253,9 @@ func _on_attack_area_body_entered(body):
 	if body.name != "TileMap":
 		print("damage taken")
 		body.take_damage(damage)
-	
+		# TODO: do this for all enemies
+		if body.name == "BringerOfDeath":
+			body.animation.play("hurt")
 
 func hit():
 	if is_damaged:
@@ -273,10 +273,8 @@ func _on_animation_tree_animation_finished(anim_name):
 		anim_tree["parameters/conditions/idle"] = true
 		is_damaged = false
 
-
 func _on_dash_timer_timeout() -> void:
 	dashing = false
-
 
 func _on_can_dash_timer_timeout() -> void:
 	canDash = true
@@ -285,45 +283,3 @@ func _on_coin_body_entered(body):
 	print("Coin collected")
 	coins+=1
 	pass 
-
-func _on_trader_body_entered(body):
-	if(body.name == "Player"):
-		%Shop.visible = true
-	pass # Replace with function body.
-
-
-func _on_trader_body_exited(body):
-	if(body.name == "Player"):
-		%Shop.visible = false
-	pass # Replace with function body.
-
-
-
-func _on_shop1_pressed(extra_arg_0):
-	if(coins >= extra_arg_0):
-		coins = coins-extra_arg_0
-		damage = damage * 1.2
-		$BuySound.play()
-	pass # Replace with function body.
-
-
-func _on_shop2_pressed(extra_arg_0):
-	if(coins >= extra_arg_0):
-		coins = coins-extra_arg_0
-		health = health * 1.2
-	pass # Replace with function body.
-
-
-func _on_button_3_pressed(extra_arg_0):
-	if(coins >= extra_arg_0):
-		coins = coins-extra_arg_0
-		health = health * 1.4
-	pass # Replace with function body.
-
-
-func _on_shop4_pressed(extra_arg_0):
-	if(coins >= extra_arg_0):
-		coins = coins-extra_arg_0
-		SPEED = SPEED * 1.2
-	pass # Replace with function body.
-
