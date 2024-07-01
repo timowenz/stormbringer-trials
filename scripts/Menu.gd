12c81c5fa8e1dc502b1572583dc4b9fe24529706
Timo Wenz <2025014@stud.hs-mannheim.de>
2024-07-01 18:40:38 +0200

    feat(controller): added controller inputs

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -5,6 +5,11 @@ extends Control
 func _ready():
 	$CreditSheet.visible = false
 
+func _process(_delta):
+	if (Input.is_action_pressed("jump")):
+		get_tree().change_scene_to_file("res://scenes/Game.tscn")
+		print("Scene changed to Game.tscn")
+
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/Game.tscn")
 	print("Scene changed to Game.tscn")
