ccf62696188577df3651b52e561b072b788b1a60
Niels Kochert <kochertN@gmail.com>
2024-07-01 16:39:11 +0200

    progress

--- scripts/EndGameCollision.gd
+++ scripts/EndGameCollision.gd
@@ -2,6 +2,7 @@ extends StaticBody2D
 
 @export var sceneName : String
 
+
 func _on_area_2d_body_entered(body):
 	if body.name == "Player" and body.can_win == true:
 		body.get_parent().get_node("WonGame").show()
