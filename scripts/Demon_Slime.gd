713e05ba6774c3692f2e00d898724de04ddbe681
Dzino3 <dzinoajax3@hotmail.com>
2024-07-02 21:15:57 +0200

    deamon_slime

--- scripts/Demon_Slime.gd
+++ scripts/Demon_Slime.gd
@@ -1,95 +1,179 @@
 extends CharacterBody2D
 
-const SPEED = 250
+enum State {IDLE, WALK, ATTACK, DEAD, HIT, CAST, SMASH, FIRE_BREATH, TRANSFORM}
 
+const SPEED = 130
+const GRAVITY = 25
+const JUMP_HEIGHT = 400
+const ENEMEY_DAMAGE = 15
 var health = 300
+var state = State.IDLE
+var player = null
+var player_chase = false
+const vulnerable = ""
+const resistance = "fire"
+var can_smash = true
+var can_cast_spell = true
+var attack_damage = 20
+var spell_damage = 50
+var smash_damage = 30
 
-var DeathSound = preload("res://assets/music/Deamon_death.mp3")
-var DeamonHit = preload("res://assets/music/Deamon_Hit.mp3")
-var DeamonSwing = preload("res://assets/music/Deamon_Swing.mp3")
-
-var dead = false
-var player_in_area = false
-var player
+@onready var anim = $AnimatedSprite2D
+@onready var healthbar = $HealthBar
+@onready var spell = preload ("res://scenes/BringerOfDeath/Spell.tscn").instantiate()
+@onready var attack_audio = $Deamon_Swing
+@onready var cast_audio = $Deamon_Swing
+@onready var smashCD = $SmashCD
+@onready var spellCD = $SpellCD
 
 func _ready():
-	$Hit_taken.hide()
-	$Give_damage.hide()
-	dead = false
+	$Node2D/Area2D/CollisionShape2D.disabled = true
+	$Node2D/Area2D/CollisionShape2D2.disabled = true
+	set_state(State.IDLE)
+	healthbar.init_health(health)
+	smashCD.connect("timeout", _on_smashcd_timeout)
+	spellCD.connect("timeout", _on_spellcd_timeout)
 
 func _physics_process(delta):
-	if !dead:
-		$DetectionArea/CollisionShape2D.disabled = false
-		if player_in_area:
-			position += (player.position - position) / SPEED
-			if(player.position.x < position.x):
-				$AnimatedSprite2D.flip_h = false
-				$Hit_taken.flip_h = false
-				$Give_damage.flip_h = false
-			else:
-				$AnimatedSprite2D.flip_h = true
-				$Hit_taken.flip_h = true
-				$Give_damage.flip_h = true
-			$AnimatedSprite2D.play("walk")
-		else:
-			$AnimatedSprite2D.play("idle")
-	if dead:
-		$DetectionArea/CollisionShape2D.disabled = true
+	match state:
+		State.IDLE:
+			idle_state(delta)
+		State.WALK:
+			walk_state(delta)
+		State.ATTACK:
+			attack_state(delta)
+		State.CAST:
+			cast_state(delta)
+		State.HIT:
+			hit_state(delta)
+		State.SMASH:
+			smash_state(delta)
+
+func hit_state(_delta):
+	if health <= 0:
+		set_state(State.DEAD)
+	else:
+		set_state(State.WALK)
+
+func set_state(new_state):
+	if state == new_state:
+		return
+	state = new_state
+	match state:
+		State.IDLE:
+			anim.play("idle")
+		State.WALK:
+			anim.play("walk")
+		State.ATTACK:
+			anim.play("attack")
+		State.HIT:
+			anim.play("hit")
+		State.DEAD:
+			anim.play("dead")
+		State.CAST:
+			anim.play("cast")
+		State.SMASH:
+			anim.play("smash")
+		State.FIRE_BREATH:
+			anim.play("fire_breath")
+		State.TRANSFORM:
+			anim.play("transform")
+
+func cast_state(_delta):
+	if not spell:
+		spell = preload("res://scenes/BringerOfDeath/Spell.tscn").instantiate()
+	spell.position = player.position
+	spell.offset = Vector2(15, -27)
+	if player.position.distance_to(position) < 200 and can_cast_spell:
+		if anim.frame == 0:
+			get_parent().add_child(spell)
+		can_cast_spell = false
+
+func _on_spellcd_timeout():
+	can_cast_spell = true
+
+func attack_state(_delta):
+	if player.position.distance_to(position) < 80:
+		if anim.frame == 4:
+			$Node2D/Area2D/CollisionShape2D.disabled = false
+			$Node2D/Area2D/CollisionShape2D2.disabled = false
+	elif player.position.distance_to(position) < 200 and can_cast_spell:
+		set_state(State.CAST)
+		can_cast_spell = false
+	elif player.position.distance_to(position) < 100 and can_smash:
+		set_state(State.SMASH)
+		can_smash = false
+	else:
+		set_state(State.WALK)
+
+func walk_state(_delta):
+	var direction = (player.position - position).normalized()
+	velocity.y += GRAVITY
+	velocity.x = direction.x * SPEED
+
+	if player.position.x < position.x:
+		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.offset.x = 0
+		$Node2D.scale.x = 1
+	else:
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.offset.x = 0
+		$Node2D.scale.x = -1
+
+	if player.position.distance_to(position) < 80:
+		set_state(State.ATTACK)
+
+	move_and_slide()
+
+func idle_state(_delta):
+	if not player_chase:
+		return
+
+	if player.position.distance_to(position) < 80:
+		set_state(State.ATTACK)
+	else:
+		set_state(State.WALK)
+
+func smash_state(_delta):
+	if player.position.distance_to(position) < 100 and can_smash:
+		set_state(State.SMASH)
+		can_smash = false
+
+func _on_smashcd_timeout():
+	can_smash = true
+
+func _on_detection_area_2d_body_entered(body):
+	player = body
+	player_chase = true
+
+func get_health():
+	return health
 
 func take_damage(damage):
-	$Deamon_Hit.stream = DeamonHit
-	health = health - damage
-	$AnimatedSprite2D.hide()
-	$Hit_taken.show()
-	$Deamon_Hit.play()
-	$Hit_taken.play("take_damage")
-	await get_tree().create_timer(0.5).timeout
-	$Hit_taken.hide()
-	$AnimatedSprite2D.show()
-	if health <= 0 and !dead :
-		death()
+	if health <= 0:
+		set_state(State.DEAD)
+		return
+	health -= damage
+	healthbar.health = health
+	set_state(State.HIT)
 
-func give_damage_to_player_from_deamon():
-	pass
+func _on_area_2d_body_entered(body):
+	if body.name == "TileMap":
+		velocity.y = -JUMP_HEIGHT
+	if body.name == "Player":
+		body.take_damage(attack_damage)
 
-func death():
-	dead = true
-	$Death_Sound.stream = DeathSound
-	$Death_Sound.play()
-	$AnimatedSprite2D.play("death")
-	await get_tree().create_timer(1.4).timeout
-	queue_free()
-
-func _on_detection_area_body_entered(body):
-	if body.has_method("player"):
-		player_in_area = true
-		player = body
-
-
-func _on_detection_area_body_exited(body):
-	if body.has_method("player"):
-		player_in_area = false
-
-
-func _on_hitbox_area_entered(area):
-	var damage
-	$Deamon_Hit.stream = DeamonHit
-	if area.has_method("give_damage"):
-		$Deamon_Hit.play()
-		damage = 50
-		take_damage(damage)
-		
-
-func give_damage_to_player():
-	$Deamon_Swing.stream = DeamonSwing
-	$Deamon_Swing.play()
-	$AnimatedSprite2D.hide()
-	$Give_damage.show()
-	$Give_damage.play("hit_player")
-	await get_tree().create_timer(1).timeout
-	$Give_damage.hide()
-	$AnimatedSprite2D.show()
-
-func _on_damage_body_entered(body):
-	if body.has_method("player"):
-		give_damage_to_player()
+func _on_animated_sprite_2d_animation_finished():
+	match anim.animation:
+		"dead":
+			queue_free()
+		"hit":
+			set_state(State.WALK)
+		"attack":
+			player.take_damage(ENEMEY_DAMAGE)
+		"cast":
+			spell.queue_free()
+			set_state(State.WALK)
+			player.take_damage(ENEMEY_DAMAGE)
+		"smash":
+			set_state(State.IDLE)
