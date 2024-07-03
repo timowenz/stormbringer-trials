88d752608e50d684115630252058021174ea97ac
Niels Kochert <48764692+vescito@users.noreply.github.com>
2024-07-03 10:22:12 +0200

    sandwich buff

--- scripts/sandwich.gd
+++ scripts/sandwich.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.name == "Player":
-		body.health += 10
+		body.health += 30
 		body.healthbar.health = body.health
 		%SandwichSound.play()
 		queue_free()
